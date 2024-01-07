d13dbe3f0c7448adacc605b1f12f767e1f9a7fe1
khaleddevfs <khaled.alhajali91@gmail.com>
2024-01-07 01:20:11 +0100

    go4lunch

---
+++ app/src/main/java/com/example/go4lunch/LoginActivity.java
@@ -0,0 +1,75 @@
+package com.example.go4lunch;
+
+import static com.example.go4lunch.LoginViewModel.RC_SIGN_IN;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import com.example.go4lunch.databinding.ActivityLoginBinding;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+
+    private LoginViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initView();
+
+        viewModel = new LoginViewModel(); // Initialisez votre viewModel ici
+
+        initListener();
+    }
+
+
+
+    private void initView() {
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    private void initListener() {
+        binding.emailLoginButton.setOnClickListener(v -> {
+            Log.d("LoginActivity", "Email Login Button Clicked");
+            viewModel.startLoginActivityEmail(LoginActivity.this);
+        });
+
+        binding.gmailLoginButton.setOnClickListener(v -> {
+            Log.d("LoginActivity", "Google Login Button Clicked");
+            viewModel.startLoginActivityGoogle(LoginActivity.this);
+        });
+
+        binding.twitterLoginButton.setOnClickListener(v -> {
+            Log.d("LoginActivity", "Twitter Login Button Clicked");
+            viewModel.startLoginActivityTwitter(LoginActivity.this);
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                viewModel.updateCurrentUser();
+                Intent loginIntent = new Intent(this, MainActivity.class);
+                startActivity(loginIntent);
+            } else {
+                Toast.makeText(this, "sign in error ", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/go4lunch/LoginViewModel.java
@@ -0,0 +1,62 @@
+package com.example.go4lunch;
+
+import android.app.Activity;
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Collections;
+
+public class LoginViewModel extends ViewModel {
+    public static final int RC_SIGN_IN = 100;
+
+
+
+    public void startLoginActivityEmail(Activity activity) {
+        Log.d("LoginActivity", "Starting Email Login Activity");
+
+        activity.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(new
+                                AuthUI.IdpConfig.EmailBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+
+    public void startLoginActivityGoogle(Activity activity) {
+        Log.d("LoginActivity", "Starting Google Login Activity");
+
+        activity.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(new
+                                AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .setIsSmartLockEnabled(false,true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    public void startLoginActivityTwitter(Activity activity) {
+        Log.d("LoginActivity", "Starting Twitter Login Activity");
+
+        activity.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(new
+                                AuthUI.IdpConfig.TwitterBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    public void updateCurrentUser(){}
+
+
+
+
+}
