4fac6bffb83dc661482edb8fd20ab0c43d37a6bc
khaleddevfs <khaled.alhajali91@gmail.com>
2024-01-08 23:08:37 +0100

    go4lunch

--- app/src/main/java/com/example/go4lunch/fragments/BaseFragment.java
+++ app/src/main/java/com/example/go4lunch/fragments/BaseFragment.java
@@ -1,19 +1,59 @@
 package com.example.go4lunch.fragments;
 
 
+
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public abstract class BaseFragment extends Fragment {
+import java.util.List;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public abstract class BaseFragment extends Fragment implements EasyPermissions.PermissionCallbacks {
+
+    private final String TAG = BaseFragment.class.getSimpleName();
+
+
+
+    protected abstract void configureFragmentOnCreateView(View view);
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        fetchUserLocation();
 
+
+    }
+
+    private void fetchUserLocation() {
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(TAG, "onRequestPermissionsResult");
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        Log.d(TAG, "onPermissionsGranted: " + perms.size());
+        fetchUserLocation();
+    }
+
+
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Log.d(TAG, "onPermissionsDenied: " + perms.size());
     }
 }
--- app/src/main/java/com/example/go4lunch/fragments/MapsFragment.java
+++ app/src/main/java/com/example/go4lunch/fragments/MapsFragment.java
@@ -1,7 +1,10 @@
 package com.example.go4lunch.fragments;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,20 +12,57 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.go4lunch.R;
+import com.example.go4lunch.databinding.FragmentMapsBinding;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapsFragment extends Fragment {
+import pub.devrel.easypermissions.EasyPermissions;
 
-    public MapsFragment() {
+public class MapsFragment extends BaseFragment implements OnMapReadyCallback, EasyPermissions.PermissionCallbacks {
 
-    }
+    private FragmentMapsBinding binding;
 
 
+    private GoogleMap googleMap;
+
+    private MapView mapView;
+
+    private Location userLocation;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentMapsBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        configureFragmentOnCreateView(view);
+        configureMapView(savedInstanceState);
+        return view;
+    }
+
+    private void configureMapView(Bundle savedInstanceState) {
+        mapView = binding.mapView;
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        googleMap.setIndoorEnabled(false);
+        updateLocationUI();
+        googleMap.setOnCameraMoveListener(this::onMarkerClick);
+    }
+
+    private void updateLocationUI() {
+    }
+
+    private void onMarkerClick() {
+
+    }
+
+    @Override
+    protected void configureFragmentOnCreateView(View view) {
+        
     }
 }
